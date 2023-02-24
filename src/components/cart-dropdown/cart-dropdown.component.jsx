5151210449e262f51dbf5b87da2989a905657922
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-02-24 07:30:51 +0530

    Added product page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,8 +1,8 @@
 import "./cart-dropdown.styles.scss"
 import Button from "../button/button.component"
 import CartItem from "../cart-item/cart-item.component"
-import { useContext } from "react"
 import {CartContext} from "../../contexts/cart.context"
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
 
 const CartDropdown = () => {
