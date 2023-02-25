c6f5d869a2029cc978820d7ad01e971ad49b4212
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-02-25 22:27:23 +0530

    Added shop page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,9 @@
 import "./cart-dropdown.styles.scss"
-import Button from "../button/button.component"
 import CartItem from "../cart-item/cart-item.component"
 import {CartContext} from "../../contexts/cart.context"
 import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+import Button from "../button/button.component"
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext)
